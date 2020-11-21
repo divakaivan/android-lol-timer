8fcbb2b4422ebdf72e6bbe0346004acf14d6e0ed
divakaivan <timee98@outlook.com>
2020-11-21 00:02:45 +0000

    add buttons for all lanes

--- app/src/main/java/com/example/loltimer/MainActivity.kt
+++ app/src/main/java/com/example/loltimer/MainActivity.kt
@@ -12,16 +12,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.top_lane)
-        button.setOnClickListener {
-            setTopTimer()
+        val topButton: Button = findViewById(R.id.top_lane)
+        val jungleButton: Button = findViewById(R.id.jungle)
+        val midButton: Button = findViewById(R.id.mid_lane)
+        val botButton: Button = findViewById(R.id.bot_lane)
+        val supButton: Button = findViewById(R.id.sup_lane)
+
+        topButton.setOnClickListener {
+            setTopTimer("Top")
+        }
+        jungleButton.setOnClickListener {
+            setTopTimer("Jungle")
+        }
+        midButton.setOnClickListener {
+            setTopTimer("Mid")
+        }
+        botButton.setOnClickListener {
+            setTopTimer("Bot")
+        }
+        supButton.setOnClickListener {
+            setTopTimer("Support")
         }
     }
 
-    private fun setTopTimer() {
-        val but: Button = findViewById(R.id.top_lane)
+    private fun setTopTimer(role: String) {
+
+        val but: Button = when (role) {
+            "Top" -> findViewById(R.id.top_lane)
+            "Jungle" -> findViewById(R.id.jungle)
+            "Mid" -> findViewById(R.id.mid_lane)
+            "Bot" -> findViewById(R.id.bot_lane)
+            "Support" -> findViewById(R.id.sup_lane)
+            else -> findViewById(R.id.top_lane)
+        }
         but.isClickable = false
-        val myText: TextView = findViewById(R.id.top_lane)
         val asd = object : CountDownTimer(3000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 val minutes = millisUntilFinished / 1000 / 60
@@ -32,11 +56,11 @@ class MainActivity : AppCompatActivity() {
                     val secondsText: String = if ("$seconds".length == 1) "0$seconds" else "$seconds"
                     "$minutes:$secondsText"
                 }
-                myText.text = "Top flash CD: $timerText"
+                but.text = "$role flash CD: $timerText"
             }
 
             override fun onFinish() {
-                myText.text = "Top Flash ready!"
+                but.text = "$role Flash ready"
                 but.isClickable = true
             }
         }
